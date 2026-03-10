ef13d2301c110e59cd2bc12248ffb87128274489
Paulina Corona <112720866+Paucb7@users.noreply.github.com>
2026-03-10 15:30:37 -0600

    Add complete Certezza Demo

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+
+
+/** 
+ * Navbar Toggle
+*/
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+    elemArr[i].addEventListener("click", function() {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
+
+/**
+ * Toggle Navbar & Overlay when clic any navbar-link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+    for (let i = 0; i < navbarLinks.length; i++) {
+    navbarLinks[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+    }
+
+
+
+
+
+    /**
+     * Header & go-to-top btn active 
+     * when windows scrolls down to 440px
+     */
+
+    const header = document.querySelector("[data-header]");
+    const goTopBtn = document.querySelector("[data-go-top]");
+
+    window.addEventListener("scroll", function(){
+        if (this.window.scrollY >= 400) {
+            header.classList.add("active");
+            goTopBtn.classList.add("active");
+        } else {
+            header.classList.remove("active");
+            goTopBtn.classList.remove("active");
+        }
+    });
+
+    /**
+     * LABEL CHANGES
+     */
+
+    const inputs = document.querySelectorAll(".input-1");
+
+    function focusFunc(){
+        let parent = this.parentNode;
+        parent.classList.add("focus");
+    }
+
+    function blurFunc(){
+        let parent = this.parentNode;
+        if(this.value == ""){
+            parent.classList.remove("focus");
+        }
+    }
+
+
+    inputs.forEach(input => {
+        input.addEventListener("focus", focusFunc);
+        input.addEventListener("blur", blurFunc);
+    });
